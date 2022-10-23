4fd3e5a9c2e0ff7e3a0785ef9dc46b60dec1e93c
AlexeyKiselyov <aleshakiselev123@gmail.com>
2022-10-23 21:03:36 +0300

    fix back link

--- src/components/AdditionalInfo/AdditionalInfo.jsx
+++ src/components/AdditionalInfo/AdditionalInfo.jsx
@@ -1,16 +1,26 @@
-import { Link } from "react-router-dom";
-import { AdditionalTitle,AdditionalList,AdditionalItem, } from "./AdditionalInfo.styled";
+import { Link, useLocation } from 'react-router-dom';
+import {
+  AdditionalTitle,
+  AdditionalList,
+  AdditionalItem,
+} from './AdditionalInfo.styled';
 
 export const AdditionalInfo = () => {
+  const location = useLocation();
+
   return (
     <>
       <AdditionalTitle>Additional information</AdditionalTitle>
       <AdditionalList>
         <AdditionalItem>
-          <Link to="cast">Cast</Link>
+          <Link to="cast" state={{ from: location.state?.from } ?? '/'}>
+            Cast
+          </Link>
         </AdditionalItem>
         <AdditionalItem>
-          <Link to="reviews">Reviews</Link>
+          <Link to="reviews" state={{ from: location.state?.from } ?? '/'}>
+            Reviews
+          </Link>
         </AdditionalItem>
       </AdditionalList>
     </>
